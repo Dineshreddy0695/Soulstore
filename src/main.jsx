9193aaf8205ba828b45440e47e1112918e6979a1
Dineshreddy0695 <dineshdinu8641@gmail.com>
2026-01-11 16:55:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ProductsProvider } from './ProductContext.jsx'
 import './index.css'
+import { HashRouter } from "react-router-dom";
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
 <ProductsProvider>
+ <HashRouter>
   <App />
+</HashRouter>
 </ProductsProvider>
   </StrictMode>,
 )
